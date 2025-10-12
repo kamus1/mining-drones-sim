71fafb3bf0ef0d9a11523c5b214397f77d9e0c54
kamus1 <benjamincamus9@gmail.com>
2025-10-11 21:59:26 -0300

    feat: fps limited to 60

--- mining-drones-sim/scenes/main.gd
+++ mining-drones-sim/scenes/main.gd
@@ -8,6 +8,9 @@ extends Node2D
 var mapa: TileMap
 
 func _ready():
+	#limitar fps a 60
+	Engine.max_fps = 60
+	
 	mapa = get_node(mapa_path)
 	_spawn_scouts()
 
@@ -19,3 +22,6 @@ func _spawn_scouts():
 		s.map_path = mapa.get_path()
 		s.start_cell = Vector2i(0, 0)  # centro de tu grilla "centrada"
 		cont.add_child(s)
+
+func _process(delta: float) -> void:
+	$CurrentFPS.text = "FPS: " + str(Engine.get_frames_per_second())
